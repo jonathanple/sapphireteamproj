f309b8698b062b70cae1e53cdf3b2432c6d2fc06
vshih258 <vshih258@gmail.com>
2025-03-26 13:19:44 -0700

    Update login.py

--- login.py
+++ login.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from time import sleep
-# from navigation import make_sidebar
-
-# make_sidebar()
 
 st.title('Team Sapphire')
 
 employee_id = st.text_input("Employee ID#")
+password = st.text_input("Password", type="password")
 
 if st.button("Log in", type="primary"):
-    if employee_id == "test":
+    if employee_id == "test" and password == "test":
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
